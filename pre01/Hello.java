f76d0905d84402374739bc032d0ce34bb68b84ba
toddorelli <96917081+toddorelli@users.noreply.github.com>
2022-01-18 23:50:03 +0000

    Added Cruel to pre01

--- pre01/Hello.java
+++ pre01/Hello.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public class Hello{
 
-public static void main(String[] args){ 
+public static void main(String[] args){
 
-System.out.println("Hello Cruel World!");
+  System.out.println("Hello Cruel World!");
 
 
 }
